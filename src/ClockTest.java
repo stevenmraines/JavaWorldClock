89e5ad114a099d3116a2a5a7e05d1051135006f4
Steven Raines <stevenmraines@gmail.com>
2017-04-27 18:39:44 -0400

    Switched to flow layout
    Switched from a grid bag layout to a flow layout so that the components
    can be dynamically rearranged. Prettied up the GUI as well.

--- src/ClockTest.java
+++ src/ClockTest.java
@@ -1,5 +1,6 @@
-import java.awt.GridBagConstraints;
-import java.awt.GridBagLayout;
+import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.FlowLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.util.TimeZone;
@@ -7,12 +8,15 @@ import java.util.TimeZone;
 import javax.swing.JComboBox;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
+import javax.swing.border.Border;
+import javax.swing.border.CompoundBorder;
+import javax.swing.border.EmptyBorder;
+import javax.swing.border.LineBorder;
 
 public class ClockTest {
 	// GUI objects
 	private static JFrame frame = new JFrame("Java World Clock");
-	private static JPanel mainPanel = new JPanel(new GridBagLayout());
-	private static JPanel[] selectorPanels = new JPanel[8];
+	private static JPanel mainPanel = new JPanel(new FlowLayout(FlowLayout.CENTER, 10, 10));
 	private static JPanel[] clockPanels = new JPanel[8];
 	private static DigitalClock[] clocks = new DigitalClock[8];
 	private static JComboBox<String> timeZoneSelector0 = new JComboBox<String>();
@@ -23,9 +27,8 @@ public class ClockTest {
 	private static JComboBox<String> timeZoneSelector5 = new JComboBox<String>();
 	private static JComboBox<String> timeZoneSelector6 = new JComboBox<String>();
 	private static JComboBox<String> timeZoneSelector7 = new JComboBox<String>();
-	private static GridBagConstraints gbc = new GridBagConstraints();
 	
-	public static void main(String[] args) {		
+	public static void main(String[] args) {
 		// Get a list of time zones
 		String[] timeZoneArray = TimeZone.getAvailableIDs();
 		
@@ -66,73 +69,41 @@ public class ClockTest {
 		timeZoneSelector6.setSelectedItem(clocks[6].getTimeZoneString());
 		timeZoneSelector7.setSelectedItem(clocks[7].getTimeZoneString());
 		
-		// Initialize selector panels and clock panels
-		for(int i = 0; i < selectorPanels.length; i++) {
-			selectorPanels[i] = new JPanel();
-			clockPanels[i] = new JPanel();
+		// Initialize clock panels
+		for(int i = 0; i < clockPanels.length; i++) {
+			clockPanels[i] = new JPanel(new BorderLayout(5, 5));
+			Border clockPanelBorder = new LineBorder(Color.LIGHT_GRAY, 1);
+			Border margin = new EmptyBorder(5, 5, 5, 5);
+			clockPanels[i].setBorder(new CompoundBorder(clockPanelBorder, margin));
 		}
 		
-		// Add selectors and clocks to their panels
-		selectorPanels[0].add(timeZoneSelector0);
-		selectorPanels[1].add(timeZoneSelector1);
-		selectorPanels[2].add(timeZoneSelector2);
-		selectorPanels[3].add(timeZoneSelector3);
-		selectorPanels[4].add(timeZoneSelector4);
-		selectorPanels[5].add(timeZoneSelector5);
-		selectorPanels[6].add(timeZoneSelector6);
-		selectorPanels[7].add(timeZoneSelector7);
-		clockPanels[0].add(clocks[0]);
-		clockPanels[1].add(clocks[1]);
-		clockPanels[2].add(clocks[2]);
-		clockPanels[3].add(clocks[3]);
-		clockPanels[4].add(clocks[4]);
-		clockPanels[5].add(clocks[5]);
-		clockPanels[6].add(clocks[6]);
-		clockPanels[7].add(clocks[7]);
+		// Add selectors and clocks to the clock panels
+		clockPanels[0].add(timeZoneSelector0, BorderLayout.NORTH);
+		clockPanels[1].add(timeZoneSelector1, BorderLayout.NORTH);
+		clockPanels[2].add(timeZoneSelector2, BorderLayout.NORTH);
+		clockPanels[3].add(timeZoneSelector3, BorderLayout.NORTH);
+		clockPanels[4].add(timeZoneSelector4, BorderLayout.NORTH);
+		clockPanels[5].add(timeZoneSelector5, BorderLayout.NORTH);
+		clockPanels[6].add(timeZoneSelector6, BorderLayout.NORTH);
+		clockPanels[7].add(timeZoneSelector7, BorderLayout.NORTH);
+		clockPanels[0].add(clocks[0], BorderLayout.CENTER);
+		clockPanels[1].add(clocks[1], BorderLayout.CENTER);
+		clockPanels[2].add(clocks[2], BorderLayout.CENTER);
+		clockPanels[3].add(clocks[3], BorderLayout.CENTER);
+		clockPanels[4].add(clocks[4], BorderLayout.CENTER);
+		clockPanels[5].add(clocks[5], BorderLayout.CENTER);
+		clockPanels[6].add(clocks[6], BorderLayout.CENTER);
+		clockPanels[7].add(clocks[7], BorderLayout.CENTER);
 		
 		// Add GUI elements to mainPanel
-		gbc.fill = GridBagConstraints.BOTH;
-		gbc.gridx = 0;
-		gbc.gridy = 0;
-		mainPanel.add(selectorPanels[0], gbc);
-		gbc.gridx = 1;
-		mainPanel.add(selectorPanels[1], gbc);
-		gbc.gridx = 2;
-		mainPanel.add(selectorPanels[2], gbc);
-		gbc.gridx = 3;
-		mainPanel.add(selectorPanels[3], gbc);
-		gbc.gridx = 0;
-		gbc.gridy = 1;
-		mainPanel.add(clockPanels[0], gbc);
-		gbc.gridx = 1;
-		mainPanel.add(clockPanels[1], gbc);
-		gbc.gridx = 2;
-		mainPanel.add(clockPanels[2], gbc);
-		gbc.gridx = 3;
-		mainPanel.add(clockPanels[3], gbc);
-		gbc.gridx = 0;
-		gbc.gridy = 2;
-		mainPanel.add(selectorPanels[4], gbc);
-		gbc.gridx = 1;
-		mainPanel.add(selectorPanels[5], gbc);
-		gbc.gridx = 2;
-		mainPanel.add(selectorPanels[6], gbc);
-		gbc.gridx = 3;
-		mainPanel.add(selectorPanels[7], gbc);
-		gbc.gridx = 0;
-		gbc.gridy = 3;
-		mainPanel.add(clockPanels[4], gbc);
-		gbc.gridx = 1;
-		mainPanel.add(clockPanels[5], gbc);
-		gbc.gridx = 2;
-		mainPanel.add(clockPanels[6], gbc);
-		gbc.gridx = 3;
-		mainPanel.add(clockPanels[7], gbc);
+		for(int i = 0; i < clockPanels.length; i++) {
+			mainPanel.add(clockPanels[i]);
+		}
 		
 		// Add everything to and setup the JFrame
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.add(mainPanel);
-		frame.pack();
+		frame.setSize(580, 520);
 		frame.setVisible(true);
 	}
 
